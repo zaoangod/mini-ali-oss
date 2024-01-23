15a8fd9224f444755837ebcd4c5a17edaa1be72a
jiayan <zaoangod@qq.com>
2024-01-24 00:22:47 +0800

    perf: 删除无用注释代码

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -5,7 +5,7 @@ export interface CustomOption {
 export async function create(url: string, method: string, data?: Record<string, any> | Array<Record<string, any>>, option?: CustomOption): Promise<Response> {
     const header: Record<string, any> = {
         'Content-Type': 'application/json',
-        ...(option?.header || {}),
+        ...(option?.header || {})
     }
     method = (method || 'GET').toUpperCase()
     const init: RequestInit = {
@@ -15,7 +15,7 @@ export async function create(url: string, method: string, data?: Record<string, 
         cache: 'no-cache',
         redirect: 'follow',
         credentials: 'same-origin',
-        referrerPolicy: 'no-referrer',
+        referrerPolicy: 'no-referrer'
     }
     console.info('init.headers:', init.headers)
     if (method == 'POST' || method == 'PUT') {
@@ -24,22 +24,6 @@ export async function create(url: string, method: string, data?: Record<string, 
     return fetch(new Request(url, init))
 }
 
-/*export async function get<T>(url: string, data: Record<string, any> = {}, option?: CustomOption): Promise<T> {
-    return create(url, 'GET', data, option).then((response: Response) => response.json())
-}
-
-export async function post<T>(url: string, data: Record<string, any> = {}, option?: CustomOption): Promise<T> {
-    return create(url, 'POST', data, option).then((response: Response) => response.json())
-}
-
-export async function put(url: string, data: Record<string, any> = {}, option?: CustomOption): Promise<Response> {
-    return create(url, 'PUT', data, option)
-}
-
-export async function del<T>(url: string, data: Record<string, any> = {}, option?: CustomOption): Promise<T> {
-    return create(url, 'DELETE', data, option).then((response: Response) => response.text())
-}*/
-
 export default {
-    create,
+    create
 }
